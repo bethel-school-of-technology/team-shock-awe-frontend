b41fd978ce167329d8ececcac967963dca26d81a
Kelli <kellishelton@gmail.com>
2021-01-19 21:35:16 -0800

    added components

--- reprise/src/app/app.module.ts
+++ reprise/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShipComponent } from './components/ship/ship.component';
+import { ProdComponent } from './components/prod/prod.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UpdateComponent } from './components/update/update.component';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShipComponent,
+    ProdComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    UpdateComponent,
+    AdminLoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
