d7d01258ded13a1c7446450ac1665aee05a409b4
Kelli <kellishelton@gmail.com>
2021-01-27 21:14:27 -0800

    added register-admin model service and json and some html

--- reprise/src/app/components/register-admin/register-admin.component.ts
+++ reprise/src/app/components/register-admin/register-admin.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Employee } from 'src/app/models/employee';
-import { EmployeeService } from 'src/app/services/employee.service';
+import { Admin } from 'src/app/models/admin';
+import { AdminService } from 'src/app/services/admin.service';
 
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms'
+
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-register-admin',
@@ -12,26 +15,28 @@ import { NgForm } from '@angular/forms'
   styleUrls: ['./register-admin.component.scss']
 })
 export class RegisterAdminComponent implements OnInit {
+  newAdmin: Admin = new Admin();
+  constructor(
+    private myAdminService: AdminService,
+    private router: Router
+  ) {}
+  
   ngOnInit(): void {
     throw new Error("Method not implemented.");
   }
 
-import { Component, OnInit } from '@angular/core';
-
-
 const Admin = new Admin()
-constructor(private myAdminService: AdminService, private Router: Router) { }
+constructor(private AdminService: AdminService, private Router: Router) { }
 
 ngOnInit(): void {
 }
 // creating the response to add admin service function
 registerAdmin(){
   console.log("testing")
-  console.log(this.newEmployee)
+  console.log(this.newAdmin)
   // created the adminService function and then subscribed to my response
   this.myAdminService.registerAdmin(this.newAdmin).subscribe(myResponse => {
     console.log(myResponse);
     this.router.navigate(["/profile"]);
   })
-}
 }
---
+++ reprise/src/app/models/admin.ts
@@ -0,0 +1,9 @@
+export class Admin {
+    firstName: string ="";
+    lastName: string ="";
+    email: string ="";
+    userName: string ="";
+    password: string ="";
+    deleted: boolean;
+    admin: boolean;
+}
