8bd4b88110fa61ff8ae449b2f42b7544a0143a86
Kelli <kellishelton@gmail.com>
2021-02-08 22:12:44 -0800

    successfully added clockin/out alert

--- reprise/src/app/components/login/login.component.ts
+++ reprise/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/services/employee.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -7,29 +8,45 @@ import { EmployeeService } from 'src/app/services/employee.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private myEmployeeService: EmployeeService) {}
+  constructor(private myEmployeeService: EmployeeService) { }
 
   loginId = "";
+  displayAlert: boolean = false;
+  alertmessage: string = "";
+  time = "";
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   clockIn() {
     this.myEmployeeService.clockIn(this.loginId).subscribe((res) => {
       console.log(res);
-      if(res.status === 200){
-        window.alert("You are clocked in!")
+      if (res.status === 200) {
+        this.alertmessage = res.message
+        this.time = res.time
+        this.displayAlert = true;
+        // window.alert("You are clocked in!")
       } else {
-        window.alert("Error clocking in")
+        this.alertmessage = res.message
+        this.displayAlert = true;
+       // window.alert("Error clocking in")
       }
     });
   }
   clockOut() {
     this.myEmployeeService.clockOut(this.loginId).subscribe((res) => {
       console.log(res);
-      if(res.status === 200){
-        window.alert("You are clocked out!")
+      if (res.status === 200) {
+        this.alertmessage = res.message
+        this.time = res.time
+        this.displayAlert = true;
+        // window.alert("You are clocked out!")
       } else {
-        window.alert("Error clocking out")
+        this.alertmessage = res.message
+        this.displayAlert = true;
+        // window.alert("Error clocking out")
       }
     });
+  }
+  toggleAlert() {
+    this.displayAlert = !this.displayAlert;
   }
 }
